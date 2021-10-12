072110d5146b8f69bc4f306f721250eec42c15f8
Daemaa <92359521+Daemaa@users.noreply.github.com>
2021-10-12 11:30:14 +0300

    Add files via upload

--- camera.js
+++ camera.js
@@ -9,18 +9,17 @@ function on_cam_error(err)
 alert("error."+err.message);
 }
 //-----------------------------------------------
-var constraints = { audio: false, video: true };
+var constraints = { audio: false, video: true, facingMode: 'environment'};
 navigator.mediaDevices.getUserMedia(constraints)
 .then(on_cam_success)
 .catch(on_cam_error);
 //-----------------------------------------------
-function capteaza()
-{
-var c = document.getElementById("canvas");
-c.width = video.width;
-c.height = video.height;
-var ctx = c.getContext("2d");
-ctx.drawImage(video, 0, 0, 640, 480);
+function capteaza(){
+    var c = document.getElementById("canvas");
+    c.width = video.width;
+    c.height = video.height;
+    var ctx = c.getContext("2d");
+    ctx.drawImage(video, 0, 0, 640, 480);
 }
 video.addEventListener("touchstart",capteaza);
 video.addEventListener("mousedown",capteaza);
